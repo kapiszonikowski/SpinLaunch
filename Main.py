e0c3fd7d8434e04fe675dcc173e34ec0f909daf6
kapiszonikowski <grzesiuk.kacper@gmail.com>
2022-12-13 17:53:58 +0100

    w trakcie dodawania siły oporu

--- Main.py
+++ Main.py
@@ -21,9 +21,26 @@ M_Z = 5.98 * 10 ** 24
 R_Z = 6371000
 G = 6.6743 * 10 ** (-11)
 V_1 = nu.sqrt(G * M_Z / R_Z)
-#air dencity formula
-def air_dencity(R_xy):
-    return np.exp(R_xy-R_Z/9.038)*1.23335 - 0.0075
+# rocket mass
+m_sl_r = 10000
+# Drag coefficient for rocket
+f = 0.03
+# Rocket cross-section
+S = np.pi * 0.5 ** 2
+
+
+# air density formula
+def air_density(height):
+    return np.exp((R_Z - float(height)) / 9.038) * 1.23335 - 0.0075
+
+
+def drag_force(height, speed):
+    if float(height) - R_Z <= 85000:
+        return float(0.5 * air_density(height) * S * speed ** 2 * f)
+    else:
+        return float(0)
+
+
 # --------------------------------------
 
 x_0 = 0.0
@@ -33,8 +50,9 @@ R_xy = (x_0 ** 2 + y_0 ** 2) ** 0.5
 k = G * M_Z
 
 Dt = 0.01
-v_x = 0
-v_y = 7900
+v_x = 0.0
+v_y = 7900.0
+v_xy = float((v_y ** 2 + v_y ** 2) ** 0.5)
 y_0 = R_Z
 x_0 = 0.0
 x_forplot = [x_0]
@@ -45,8 +63,8 @@ listR_xy = [R_Z]
 for i in range(500000):
     R_xy = (X_location ** 2 + Y_location ** 2) ** 0.5
     listR_xy.append(R_xy)
-    a_x = -k * X_location * R_xy ** (-3)
-    a_y = -k * Y_location * R_xy ** (-3)
+    a_x = -k * X_location * R_xy ** (-3) - drag_force(R_xy, v_x)
+    a_y = -k * Y_location * R_xy ** (-3) - drag_force(R_xy, v_y)
     if i % 1 == 0:
         x_forplot.append(X_location)
         y_forplot.append(Y_location)
@@ -61,12 +79,9 @@ for i in range(500000):
         print(
             f'a_x: {round(a_x, 2)} m/s^2,  ay: {round(a_y, 2)} m/s^2,  R: {round(R_xy)} m,  t: {round(i / 3600000, 2)} h')
         print("Location (x,y):", [round(X_location, 0), round(Y_location)], "m\n")'''
-maxR_xy = max(R_xy)
 
 
 plt.plot(x_forplot, y_forplot, color='g')
 theta = np.arange(0, np.pi * 2, 0.01)
 plt.plot(R_Z * np.cos(theta), R_Z * np.sin(theta), color='b')
 plt.show()
-
-

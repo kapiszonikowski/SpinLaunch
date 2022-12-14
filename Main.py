a3a921e4ca42944ed3dfdf4935878766659ba342
kapiszonikowski <grzesiuk.kacper@gmail.com>
2022-12-14 10:09:26 +0100

    siła oporu dodana

--- Main.py
+++ Main.py
@@ -34,7 +34,7 @@ def air_density(height):
     return nu.exp(((R_Z - height) / 1000 / 9.038)) * 1.23335 - 0.0075
 
 
-def drag_altitude(height, speed):
+def drag_acceleration(height, speed):
     if float(height) - R_Z <= 85000:
         return 0.5 * air_density(height) * S * speed ** 2 * f / m_sl_r
     else:
@@ -66,8 +66,8 @@ droga = 0
 for i in range(90000):
     R_xy = (X_location ** 2 + Y_location ** 2) ** 0.5
     listR_xy.append(R_xy)
-    a_x = -k * X_location * R_xy ** (-3) - drag_altitude(R_xy, v_x)
-    a_y = -k * Y_location * R_xy ** (-3) - drag_altitude(R_xy, v_y)
+    a_x = -k * X_location * R_xy ** (-3) - drag_acceleration(R_xy, v_x)
+    a_y = -k * Y_location * R_xy ** (-3) - drag_acceleration(R_xy, v_y)
     x_forplot.append(X_location)
     y_forplot.append(Y_location)
     v_x = v_x + a_x * Dt
@@ -94,7 +94,6 @@ n = -0.8*R_Z
 plt.plot(x_forplot, y_forplot, color='g')
 theta = np.arange(0, np.pi * 2, 0.01)
 plt.plot(R_Z * np.cos(theta), R_Z * np.sin(theta), color='b')
-#plt.text(-0.2 * R_Z, 0.3 * R_Z, 'z tarciem', fontsize=10)
 plt.text(n, 0.2 * R_Z, napis_prędkości, fontsize=10)
 plt.text(n, 0.1 * R_Z, napis_drogi, fontsize=10)
 plt.text(n, 0.0 * R_Z, napis_wysokości, fontsize=10)

92b78ef02f3d65b212711280c73227d3740f2c03
kapiszonikowski <grzesiuk.kacper@gmail.com>
2022-12-14 01:26:45 +0100

    siła oporu dodana

--- Imlpementacja_wykresu.py
+++ Imlpementacja_wykresu.py
@@ -1,24 +1,61 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import numpy as nu
+import matplotlib.pyplot as plt
+import os
+import imageio
+import time
 
-# Fixing random state for reproducibility
-np.random.seed(19680801)
+'''
+x_0 = float(input())
+y_0 = float(input())
+R_Z = float(input())
+v_x = float(input())
+v_y = float(input())
+V = [v_x,v_y]
+kont = nu.angle(float(input()), angle = True)
+'''
+# V = [v_x, v_y]
+# v_x = float(input("Podaj wartość składowej x-owej prędkości początkowej w metrach na sekundę"))
 
 
-N = 100
-r0 = 0.6
-x = np.random.rand(N)
-y = np.random.rand(N)
-area = (20 * np.random.rand(N))**2  # 0 to 10 point radii
-c = 1000 * np.sqrt(area)
-print(c)
-r = np.sqrt(x ** 2 + y ** 2)
-area1 = np.ma.masked_where(r < r0, area)
-area2 = np.ma.masked_where(r >= r0, area)
-plt.scatter(x, y, s=area1, marker='^', c=c)
-plt.scatter(x, y, s=area2, marker='o', c=c)
-# Show the boundary between the regions:
-theta = np.arange(0, np.pi / 2, 0.01)
-plt.plot(r0 * np.cos(theta), r0 * np.sin(theta))
+# -------------------------------------
+M_Z = 5.98 * 10 ** 24
+R_Z = 6371000
+G = 6.6743 * 10 ** (-11)
+V_1 = nu.sqrt(G * M_Z / R_Z)
+# rocket mass
+m_sl_r = 10000
+# Drag coefficient for rocket
+f = 0.03
+# Rocket cross-section
+S = np.pi * 0.5 ** 2
 
-plt.show()
+
+
+droga = 1
+listR_xy = [1]
+v_x0 = 1
+v_y0 = 1
+#dodawanie napisów do wykresu
+nd = round((droga / 1000), 2)
+nw = round((max(listR_xy) - R_Z) / 1000, 2)
+napis_drogi = f'droga przebyta przez ciao: {nd}km'
+napis_wysokości = f'maksymalna wysokość ciała n.p.k: {nw}km'
+napis_prędkości = f'początkowe prędkości ciał [km/s]: Vx = {round(v_x0/1000), 2} ; Vy = {round(v_y0/1000), 2}'
+
+
+#dodawanie wykresu
+theta = np.arange(0, np.pi * 2, 0.01)
+plt.plot(R_Z * np.cos(theta), R_Z * np.sin(theta), color='b')
+plt.text(-0.2 * R_Z, 0.3 * R_Z, 'Z siłami oporu', fontsize=10)
+plt.text(-0.9 * R_Z, 0.2 * R_Z, napis_prędkości, fontsize=10)
+plt.text(-0.9 * R_Z, 0.1 * R_Z, napis_drogi, fontsize=10)
+plt.text(-0.9 * R_Z, 0.0 * R_Z, napis_wysokości, fontsize=10)
+my_path = 'C:/Users/pc/Desktop\Жизнь\Studia\SEM III\Wstęp do modelowania zjawisk fizycznych\Projekt - model spinlaunch|'
+my_file = "test.jpg"
+dir_name = "C:/Windows/Temp/"
+
+plt.rcParams["savefig.directory"] = os.chdir(os.path.dirname(dir_name))
+
+
+
--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import numpy as nu
 import matplotlib.pyplot as plt
-import time
+
 
 '''
 x_0 = float(input())
@@ -31,14 +31,14 @@ S = np.pi * 0.5 ** 2
 
 # air density formula
 def air_density(height):
-    return np.exp((R_Z - float(height)) / 9.038) * 1.23335 - 0.0075
+    return nu.exp(((R_Z - height) / 1000 / 9.038)) * 1.23335 - 0.0075
 
 
-def drag_force(height, speed):
+def drag_altitude(height, speed):
     if float(height) - R_Z <= 85000:
-        return float(0.5 * air_density(height) * S * speed ** 2 * f)
+        return 0.5 * air_density(height) * S * speed ** 2 * f / m_sl_r
     else:
-        return float(0)
+        return 0
 
 
 # --------------------------------------
@@ -50,8 +50,10 @@ R_xy = (x_0 ** 2 + y_0 ** 2) ** 0.5
 k = G * M_Z
 
 Dt = 0.01
-v_x = 0.0
-v_y = 7900.0
+v_x0 = 1000
+v_y0 = 1500
+v_x = v_x0
+v_y = v_y0
 v_xy = float((v_y ** 2 + v_y ** 2) ** 0.5)
 y_0 = R_Z
 x_0 = 0.0
@@ -60,28 +62,38 @@ y_forplot = [y_0]
 X_location = x_0
 Y_location = y_0
 listR_xy = [R_Z]
-for i in range(500000):
+droga = 0
+for i in range(50000):
     R_xy = (X_location ** 2 + Y_location ** 2) ** 0.5
     listR_xy.append(R_xy)
-    a_x = -k * X_location * R_xy ** (-3) - drag_force(R_xy, v_x)
-    a_y = -k * Y_location * R_xy ** (-3) - drag_force(R_xy, v_y)
-    if i % 1 == 0:
-        x_forplot.append(X_location)
-        y_forplot.append(Y_location)
+    a_x = -k * X_location * R_xy ** (-3) - drag_altitude(R_xy, v_x)
+    a_y = -k * Y_location * R_xy ** (-3) - drag_altitude(R_xy, v_y)
+    x_forplot.append(X_location)
+    y_forplot.append(Y_location)
     v_x = v_x + a_x * Dt
     v_y = v_y + a_y * Dt
     if R_xy < R_Z:
         break
     X_location = X_location + v_x * Dt + 0.5 * a_x * Dt ** 2
     Y_location = Y_location + v_y * Dt + 0.5 * a_y * Dt ** 2
-    location = [X_location, Y_location]
+    droga = droga + ((x_forplot[i] - x_forplot[i - 1]) ** 2 + (y_forplot[i] - y_forplot[i - 1]) ** 2) ** 0.5
     '''if i % 4000 == 0:
         print(
             f'a_x: {round(a_x, 2)} m/s^2,  ay: {round(a_y, 2)} m/s^2,  R: {round(R_xy)} m,  t: {round(i / 3600000, 2)} h')
         print("Location (x,y):", [round(X_location, 0), round(Y_location)], "m\n")'''
 
+nd = round((droga / 1000), 2)
+nw = round((max(listR_xy) - R_Z) / 1000, 2)
+napis_drogi = f'droga przebyta przez ciao: {nd}km'
+napis_wysokości = f'maksymalna wysokość ciała n.p.k: {nw}km'
+napis_prędkości = f'początkowe prędkości ciał [km/s]: Vx = {round(v_x0/1000, 2)} ; Vy = {round(v_y0/1000, 2)}'
 
+#dodawanie wykresu
 plt.plot(x_forplot, y_forplot, color='g')
 theta = np.arange(0, np.pi * 2, 0.01)
 plt.plot(R_Z * np.cos(theta), R_Z * np.sin(theta), color='b')
+plt.text(-0.9 * R_Z, 0.2 * R_Z, napis_prędkości, fontsize=10)
+plt.text(-0.6 * R_Z, 0.1 * R_Z, napis_drogi, fontsize=10)
+plt.text(-0.6 * R_Z, 0.0 * R_Z, napis_wysokości, fontsize=10)
 plt.show()
+
